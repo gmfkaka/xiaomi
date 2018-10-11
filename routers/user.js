65392b9759faf583ede8ebd572e06ba15eab87e4
gmfkaka <542915779@qq.com>
2018-10-11 20:05:29 +0800

    register verification

--- routers/user.js
+++ routers/user.js
@@ -6,10 +6,10 @@ var router = express.Router();
 router.post('/login',(req,res)=>{
     var obj = req.body;
     //console.log(obj);
-    var uname = obj.uname;
-    var upwd = obj.upwd;
-    var sql = `SELECT * FROM mi_user WHERE uname=? AND upwd=?`;
-    pool.query(sql,[uname,upwd],(err,result)=>{
+    var phone = obj.uname;
+    var pwd = obj.upwd;
+    var sql = `SELECT * FROM mi_user WHERE phone=? AND pwd=?`;
+    pool.query(sql,[phone,pwd],(err,result)=>{
         if(err) throw err;
         if(result.length>0){
             res.send({code:1,msg:"登录成功"});
@@ -17,6 +17,22 @@ router.post('/login',(req,res)=>{
             res.send({code:0,msg:"登录失败"})
         }
     });
+});
+
+//2.用户注册
+router.post('/register',(req,res)=>{
+    var obj = req.body;
+    var phone = obj.phone;
+    var pwd = obj.pwd;
+    var sql = `INSERT INTO mi_user VALUES(NUll,?,?)`;
+    pool.query(sql,[phone,pwd],(err,result)=>{
+        if(err) throw err;
+        if(result.affectedRows>0){
+            res.send({code:1,msg:"注册成功"});
+        }else{
+            res.send({code:0,msg:"注册失败"})
+        }
+    })
 })
 //导出模块
 module.exports = router;
